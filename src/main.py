9fedd591d07ee7676f355303fea53bba896598bd
Lucas Brito <lucasbsilva29@gmail.com>
2024-04-10 22:14:57 -0300

    refactor: Improve TCX file export and formatting

--- src/main.py
+++ src/main.py
@@ -2,26 +2,33 @@ import re
 import os
 import sys
 import logging
-from xml.dom import minidom  # Import minidom for XML indentation
+from xml.dom import minidom
 from typing import List
 
 import questionary
+import webbrowser
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 if not logger.handlers:
     logging.basicConfig(level=logging.INFO)
-    handler = logging.StreamHandler(sys.stdout)
+    handler = logging.FileHandler('logs.log')
     formatter = logging.Formatter('%(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
 
 def main():
-    file_path = ask_file_path()
     sport = ask_sport()
-
     logger.info(f"Selected sport: {sport}")
+
+    activity_id = ask_activity_id()
+    logger.info(f"Selected activity ID: {activity_id}")
+    logger.info("Downloading the TCX file from strava")
+    download_tcx_file(activity_id, sport)
+
+    file_path = ask_file_path()
+
     if sport in ["Swim", "Other"]:
         logger.info("Formatting the TCX file to be imported to trainingpeaks")
         format_to_swim(file_path)
@@ -33,21 +40,42 @@ def main():
         logger.error("Invalid sport selected")
         sys.exit(1)
 
-    indent_xml_file(file_path)  # Indent the XML file after modifications
+    indent_xml_file(file_path)
     logger.info("Done!")
-
-
-def ask_file_path() -> str:
-    return questionary.text(
-        "Enter the path to the TCX file you want to export to trainingpeaks",
-        validate=os.path.isfile
-    ).ask()
 
 
 def ask_sport() -> str:
     return questionary.select(
         "What sport do you want to export to trainingpeaks?",
         choices=["Bike", "Run", "Swim", "Other"]
+    ).ask()
+
+
+def ask_activity_id() -> str:
+    return questionary.text(
+        "Enter the strava activity ID you want to export to trainingpeaks:"
+    ).ask()
+
+
+def download_tcx_file(activity_id: str, sport: str) -> None:
+    if sport in ["Swim", "Other"]:
+        url = f"https://www.strava.com/activities/{activity_id}/export_original"
+    else:
+        url = f"https://www.strava.com/activities/{activity_id}/export_tcx"
+    try:
+        webbrowser.open(url)
+    except Exception as e:
+        logger.error(
+            "It was not possible to download the TCX file from strava."
+        )
+        logger.error(f"Error opening the browser: {e}.")
+        sys.exit(1)
+
+
+def ask_file_path() -> str:
+    return questionary.text(
+        "Check if the TCX file was downloaded and then enter the path to the file:",
+        validate=os.path.isfile
     ).ask()
 
 
@@ -78,13 +106,18 @@ def write_xml_file(file_path: str, xml_str: str) -> None:
 
 
 def indent_xml_file(file_path: str) -> None:
-    with open(file_path, "r") as xml_file:
-        xml_content = xml_file.read()
+    try:
+        with open(file_path, "r") as xml_file:
+            xml_content = xml_file.read()
 
-    xml_dom = minidom.parseString(xml_content)
+        xml_dom = minidom.parseString(xml_content)
 
-    with open(file_path, "w") as xml_file:
-        xml_file.write(xml_dom.toprettyxml(indent="  "))
+        with open(file_path, "w") as xml_file:
+            xml_file.write(xml_dom.toprettyxml(indent="  "))
+    except Exception as e:
+        logger.warning(
+            "It was not possible to indent the XML file. The file will be saved without indentation."
+        )
 
 
 if __name__ == "__main__":
