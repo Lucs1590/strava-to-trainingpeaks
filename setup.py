aca8ea2bf2dfc03c83e0c4068e9c907125696f13
Lucas Brito <lucasbsilva29@gmail.com>
2024-12-14 11:19:04 -0300

    chore: add __version__.py file with version 1.0.0

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup, find_packages
 setup(
     name="strava-to-trainingpeaks",
-    version="1.1.0",
+    version=re.findall(
+        re.compile(r'[0-9]+\.[0-9]+\.[0-9]+'),
+        read('__version__.py')
+    )[0],
     author="Lucas de Brito Silva",
     author_email="lucasbsilva29@gmail.com",
     description="A tool to sync Strava activities with TrainingPeaks, with the OpenAI API creating the workout descriptions.",
     packages=find_packages(),
     install_requires=[
         "defusedxml==0.7.1",
-            "langchain_core==0.3.22",
-            "langchain_openai==0.2.11",
-            "numpy==1.26.4",
-            "pandas==2.2.3",
-            "python-dotenv==1.0.1",
-            "questionary==2.0.1",
-            "scipy==1.13.1",
-            "tcxreader==0.4.10",
-            "tqdm==4.67.1"
+        "langchain_core==0.3.22",
+        "langchain_openai==0.2.11",
+        "numpy==1.26.4",
+        "pandas==2.2.3",
+        "python-dotenv==1.0.1",
+        "questionary==2.0.1",
+        "scipy==1.13.1",
+        "tcxreader==0.4.10",
+        "tqdm==4.67.1"
     ],
     entry_points={
         "console_scripts": [
